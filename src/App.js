4ad19a14c8581ca7fd049e101bb853f13990f257
Gaganmeet Bahri <gaganmeet@hybrowlabs.com>
2022-01-30 21:24:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="bg-neutral-800 min-h-screen flex flex-col items-center justify-center text-white">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="text-2xl font-bold text-red-800">Tailwind 3 With React Js</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
